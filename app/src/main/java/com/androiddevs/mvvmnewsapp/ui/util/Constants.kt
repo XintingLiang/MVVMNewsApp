72fde9247d03aef1f2cfb85859d6c1ab40b7e150
Xinting Liang <lily800905@gmail.com>
2021-03-18 16:03:47 -0700

    set up api Web Request
    1. interface to get breaking news and search for news
    2. created retrofit instance to connect with interface
    3. used JSON to Kotlin plugins to auto generate data class based on the api structure
    (news response, source, article)

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+class Constants {
+    companion object{
+        const val API_KEY ="c485481ee10c496e8d2bd4ca34ce0aba"
+        const val BASE_URL="https://newsapi.org"
+    }
+}
