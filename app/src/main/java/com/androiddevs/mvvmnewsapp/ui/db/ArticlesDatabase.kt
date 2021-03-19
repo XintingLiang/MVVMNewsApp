6fc363ab0cf5a0ce002409a702ce0a3315b198f6
Xinting Liang <lily800905@gmail.com>
2021-03-18 17:07:12 -0700

    Dao && Database && converters
    @TypeConverters to  convert source class's name into String, so we can tell the room database to store it as string
    
    after using @TypeConverters we need to put it in the database class too

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/db/ArticlesDatabase.kt
@@ -0,0 +1,39 @@
+package com.androiddevs.mvvmnewsapp.ui.db
+
+import android.content.Context
+import androidx.room.*
+import com.androiddevs.mvvmnewsapp.ui.model.Article
+
+
+@Database(entities = [Article::class],version = 1)
+@TypeConverters(Converters::class)
+
+abstract class ArticlesDatabase: RoomDatabase() {
+
+    abstract fun getArticleDao (): ArticleDao
+
+
+    companion object{
+
+        // WE create this instance object which is a singleton so we are sure there will be only 1 instance in this database
+        // Volatile will make sure whenever a thread update the database, other threads will see the update instantly
+        @Volatile
+        private var instance : ArticlesDatabase? = null
+        private val LOCK = Any()//? will be used in the synchronized block to make sure we will only have one instance
+
+        // whenever we create the instance in this database, this invoke function will be called,
+        // we will check if the instance is a null, if it's a null, we execute the things inside of the synchronized block
+        // we check the instance in the synchronized block again to see if the instance is a null
+        // if it's a null, we call the createDatabase function to create one instance and return the value to instance
+        operator fun invoke(context: Context) = instance?: synchronized(LOCK){
+            instance?:createDatabase(context).also{ instance=it}
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(
+                context.applicationContext,
+                ArticlesDatabase::class.java,
+                "article_db.db"
+            ).build()
+    }
+}// this instance of the database will be used to access the articleDao, which has the actual database functions
