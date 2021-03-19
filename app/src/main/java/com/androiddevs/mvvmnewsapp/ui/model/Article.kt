22723dbfc68bfd6aed3d51b72d3383114b2fcaa9
Xinting Liang <lily800905@gmail.com>
2021-03-18 17:04:51 -0700

    organize auto generated dataclass, and put them in model folder

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/model/Article.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/model/Article.kt
@@ -1,6 +1,12 @@
-package com.androiddevs.mvvmnewsapp.ui
+package com.androiddevs.mvvmnewsapp.ui.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "articles")
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int? = null,
     val author: String,
     val content: String,
     val description: String,
