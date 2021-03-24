74998632e34ae2f6b3f557014634f4ea364c1955
Xinting Liang <lily800905@gmail.com>
2021-03-24 10:50:08 -0700

    rename Resource to State

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
@@ -1,22 +1,20 @@
 package com.androiddevs.mvvmnewsapp.ui.ui
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.androiddevs.mvvmnewsapp.ui.model.Article
 import com.androiddevs.mvvmnewsapp.ui.model.NewsResponse
-import com.androiddevs.mvvmnewsapp.ui.model.Source
-import com.androiddevs.mvvmnewsapp.ui.util.Resource
+import com.androiddevs.mvvmnewsapp.ui.util.State
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
 class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
 
-    val breakingNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val breakingNews: MutableLiveData<State<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
 
-    val searchNews: MutableLiveData<Resource<NewsResponse>> = MutableLiveData()
+    val searchNews: MutableLiveData<State<NewsResponse>> = MutableLiveData()
     var searchNewsPage = 1
 
     init {
@@ -25,36 +23,36 @@ class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
 
     // connect to repository to api service
     fun getBreakingNews (countryCode: String)= viewModelScope.launch {
-         breakingNews.postValue(Resource.Loading())
+         breakingNews.postValue(State.Loading())
         val response = newsRepository.getBreakingNews(countryCode,breakingNewsPage)
         println("baobaoissmart.response: ${response.body()}")
         val handleResponse = handleBreakingNewsResponse(response)
         breakingNews.postValue(handleResponse)
         }
 
-    fun handleBreakingNewsResponse(response: Response<NewsResponse>):Resource<NewsResponse> {
+    fun handleBreakingNewsResponse(response: Response<NewsResponse>):State<NewsResponse> {
         // why do we need to use Resource class when we also check if it's successful here?
         if (response.isSuccessful){
             response.body()?.let { resultResponse->
-                return Resource.Success(resultResponse)
+                return State.Success(resultResponse)
             }
         }
-        return Resource.Error(response.message())//?
+        return State.Error(response.message())//?
     }// can we use enqueue(object: Callback) here to handle onResponse or onFailure??
 
     fun searchForNews(searchQuery: String) = viewModelScope.launch {
-        searchNews.postValue(Resource.Loading())
+        searchNews.postValue(State.Loading())
         val response = newsRepository.searchForNews(searchQuery,searchNewsPage)
         searchNews.postValue(handleSearchNews(response))
     }
 
-    fun handleSearchNews(response: Response<NewsResponse>): Resource<NewsResponse>{
+    fun handleSearchNews(response: Response<NewsResponse>): State<NewsResponse>{
         if (response.isSuccessful){
            response.body()?.let { resultResponse->
-               return Resource.Success(resultResponse)
+               return State.Success(resultResponse)
            }
         }
-        return Resource.Error(response.message())
+        return State.Error(response.message())
     }
 
     fun getSavedArticle()= newsRepository.getSavedArticle()
@@ -63,5 +61,7 @@ class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
         newsRepository.deleteArticle(article)  }
 
     fun upsertArticle(article: Article)= viewModelScope.launch {
-        newsRepository.upsertArticle(article) }
+        val whyLong = newsRepository.upsertArticle(article)
+        println("baobao.upsertArticle.why not short.why long? long: $whyLong")
+    }
 }
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/State.kt
@@ -0,0 +1,11 @@
+package com.androiddevs.mvvmnewsapp.ui.util
+
+//<T> refers to any type
+sealed class State<T>(
+    val data : T? = null,
+    val message : String? = null
+) {
+    class Success<T>(data: T) : State<T>(data)
+    class Error<T>(message: String, data: T?= null): State<T>(data, message)
+    class Loading<T>: State<T>()
+}
