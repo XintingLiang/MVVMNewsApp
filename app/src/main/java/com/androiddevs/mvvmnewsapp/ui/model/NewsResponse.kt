df344460ebc28588d1dad76374d9aed015981845
Xinting Liang <lily800905@gmail.com>
2021-03-24 17:12:14 -0700

    changed Article to MutableList so we can add later when scroll
     if the type we are editing is not primitive, when we edit, we are editing the original even we make a copy of that, see viewModel new&&old article

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/model/NewsResponse.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/model/NewsResponse.kt
@@ -3,7 +3,7 @@ package com.androiddevs.mvvmnewsapp.ui.model
 import com.androiddevs.mvvmnewsapp.ui.model.Article
 
 data class NewsResponse(
-    val articles: List<Article>,
+    val articles: MutableList<Article>,
     val status: String,
     val totalResults: Int
 )
