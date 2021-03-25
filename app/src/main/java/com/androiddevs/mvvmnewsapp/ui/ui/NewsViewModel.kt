bfd604236dc82ced2a6ba0be06a89e9fd7d44062
Xinting Liang <lily800905@gmail.com>
2021-03-24 17:14:34 -0700

    paging in viewModel
    breakingNews paging && searchForNews paging

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/ui/NewsViewModel.kt
@@ -13,9 +13,11 @@ class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
 
     val breakingNews: MutableLiveData<State<NewsResponse>> = MutableLiveData()
     var breakingNewsPage = 1
+    var breakingNewsResponse : NewsResponse? = null
 
     val searchNews: MutableLiveData<State<NewsResponse>> = MutableLiveData()
     var searchNewsPage = 1
+    var searchNewsResponse : NewsResponse? = null
 
     init {
         getBreakingNews("us")
@@ -24,32 +26,55 @@ class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
     // connect to repository to api service
     fun getBreakingNews (countryCode: String)= viewModelScope.launch {
          breakingNews.postValue(State.Loading())
-        val response = newsRepository.getBreakingNews(countryCode,breakingNewsPage)
-        println("baobaoissmart.response: ${response.body()}")
+        val response = newsRepository.getBreakingNews(countryCode,breakingNewsPage)// first page result
         val handleResponse = handleBreakingNewsResponse(response)
         breakingNews.postValue(handleResponse)
         }
 
     fun handleBreakingNewsResponse(response: Response<NewsResponse>):State<NewsResponse> {
-        // why do we need to use Resource class when we also check if it's successful here?
+        // after we got the first page NewsResponse, we load the second page, but when do we call second page request?
+        // we will call the request in the UI when we are scrolling and isNotTheLastPage and total>visible
         if (response.isSuccessful){
             response.body()?.let { resultResponse->
-                return State.Success(resultResponse)
+                breakingNewsPage++
+
+                val currentBreakingNewsResponse = breakingNewsResponse
+                if (currentBreakingNewsResponse==null){
+                    breakingNewsResponse= resultResponse
+                    return State.Success(resultResponse)
+                } else{
+                    val oldArticles = breakingNewsResponse?.articles
+                    val newArticles = resultResponse.articles
+                    oldArticles?.addAll(newArticles)
+
+                    // the above is the same as following: if the type we are editing is not primitive, when we edit, we are editing the original even
+                    // we make a copy of that
+                   // breakingNewsResponse?.articles?.addAll(resultResponse.articles)
+                    return State.Success(currentBreakingNewsResponse)
+                }
             }
         }
         return State.Error(response.message())//?
-    }// can we use enqueue(object: Callback) here to handle onResponse or onFailure??
+    }// can we use enqueue(object: Callback) here to handle onResponse or onFailure?? we can, but we also want to have a loading state, so we use State
 
     fun searchForNews(searchQuery: String) = viewModelScope.launch {
         searchNews.postValue(State.Loading())
-        val response = newsRepository.searchForNews(searchQuery,searchNewsPage)
+        val response = newsRepository.searchForNews(searchQuery,searchNewsPage)// first page result
         searchNews.postValue(handleSearchNews(response))
     }
 
     fun handleSearchNews(response: Response<NewsResponse>): State<NewsResponse>{
         if (response.isSuccessful){
            response.body()?.let { resultResponse->
-               return State.Success(resultResponse)
+               searchNewsPage++
+               if(searchNewsResponse==null){
+                   searchNewsResponse=resultResponse
+               }else{
+                   val oldArticle = searchNewsResponse?.articles
+                   val newArticle = resultResponse.articles
+                   oldArticle?.addAll(newArticle)
+               }
+               return State.Success(searchNewsResponse?:resultResponse)// what do we return here? did we add the old articles and new article? did we pass it ?
            }
         }
         return State.Error(response.message())
@@ -61,7 +86,6 @@ class NewsViewModel (val newsRepository: NewsRepository): ViewModel(){
         newsRepository.deleteArticle(article)  }
 
     fun upsertArticle(article: Article)= viewModelScope.launch {
-        val whyLong = newsRepository.upsertArticle(article)
-        println("baobao.upsertArticle.why not short.why long? long: $whyLong")
+         newsRepository.upsertArticle(article)
     }
 }
