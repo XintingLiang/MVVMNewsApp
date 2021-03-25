9e1023f831640d642a24b6afb6a516efbbdfca76
Xinting Liang <lily800905@gmail.com>
2021-03-24 17:17:01 -0700

    when to paginate, and when it's the last page, stop paginate
    Questions:
    data from api, how many?
    data displayed in recycleView, how many per page
    why total/ Query page size +2 is the total page
    why use Query_page_size

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/fragments/BreakingNewsFragment.kt
@@ -3,14 +3,17 @@ package com.androiddevs.mvvmnewsapp.ui.fragments
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.AbsListView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.androiddevs.mvvmnewsapp.R
 import com.androiddevs.mvvmnewsapp.ui.adapter.NewsAdapter
 import com.androiddevs.mvvmnewsapp.ui.ui.NewsActivity
 import com.androiddevs.mvvmnewsapp.ui.ui.NewsViewModel
+import com.androiddevs.mvvmnewsapp.ui.util.Constants.Companion.QUERY_PAGE_SIZE
 import com.androiddevs.mvvmnewsapp.ui.util.State
 import kotlinx.android.synthetic.main.fragment_breaking_news.*
 
@@ -34,7 +37,13 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
                is State.Success-> {
                    hideProgressBar()
                    resourceResponse.data?.let {newsReponse->
-                       newsAdapter.diff.submitList(newsReponse.articles)
+                       newsAdapter.diff.submitList(newsReponse.articles.toList())
+
+                       // if it's the last page already, we don't paginate any more
+                       // total result is the result we get from the api NewsResponse
+                       // when we divide, we round off, that's 1, and the last page should always be empty, so we add 2
+                       val totalPage = newsReponse.totalResults / QUERY_PAGE_SIZE +2
+                       isLastPage = viewModel.breakingNewsPage == totalPage
                    }
                }
 
@@ -53,22 +62,59 @@ class BreakingNewsFragment: Fragment(R.layout.fragment_breaking_news) {
 
     }
 
-
     fun hideProgressBar (){
         paginationProgressBar.visibility = View.INVISIBLE
-
+        isLoading = false
     }
 
     fun showProgressBar(){
         paginationProgressBar.visibility= View.VISIBLE
+        isLoading = true
     }
-
 
     fun setUpRecyclerView (){
         newsAdapter=NewsAdapter()
         rvBreakingNews.apply{
             adapter=newsAdapter
             layoutManager=LinearLayoutManager(activity)
+            addOnScrollListener(this@BreakingNewsFragment.scrollListener)// apply scrollListener
         }
     }
+
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    val scrollListener = object: RecyclerView.OnScrollListener(){
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            // if we are scrolling
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL){
+                 isScrolling=true
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+
+            val isNotLoadingAndNotLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= QUERY_PAGE_SIZE
+            val shouldPaginate = isNotLoadingAndNotLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible
+                    &&isScrolling
+
+            if (shouldPaginate){
+                viewModel.getBreakingNews("us")
+                isScrolling = false
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY ="c485481ee10c496e8d2bd4ca34ce0aba"
         const val BASE_URL="https://newsapi.org"
         const val SEARCH_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
